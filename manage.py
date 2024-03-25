2469038a740de14a9c6955b3d77b38f01af7afe1
Mohamed Nasser <nassermohamed3222@gmail.com>
2024-03-25 23:24:59 +0200

    Add logging and replace print statements

--- manage.py
+++ manage.py
@@ -1,6 +1,11 @@
 import os
 import sys
 import venv
+import logging
+
+#logging root configuration
+logging.basicConfig(level=logging.DEBUG, # This will log all levels of messages
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def distribute():
     """Create new version on PyPI
@@ -13,13 +18,17 @@ def distribute():
     You need: PyPI username and password.
     You need to type in the PyPI password rather than copy-pasting.
     """
-
+    create_venv()
     install()
 
     os.system(activate() + ' && ' + 'pip install --upgrade build')
     os.system(activate() + ' && ' + 'python -m build')
     os.system(activate() + ' && ' + 'pip install --upgrade twine')
     os.system(activate() + ' && ' + 'twine upload dist/*')
+
+def create_venv():
+    logging.info("Creating virtual environment...")
+    os.system('py -3 -m venv .venv')
 
 def activate():
     """Active virtual environment"""
@@ -36,10 +45,7 @@ def activate():
 def install():
     """Install requirements to a virtual environment"""
 
-    print('Creating virtual environment..')
-    os.system('py -3 -m venv .venv')
-
-    print('Installing requirements..')
+    logging.info("Installing requirements...")
     os.system(activate() + ' && ' + 'py -m pip install -r requirements.txt')
 
 
