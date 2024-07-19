eb9b39f6ae79fcff925250780eb4cfe973f7d01f
Mohamed Nasser <nassermohamed3222@gmail.com>
2024-07-19 21:47:02 +0300

    Create forward tofts function

--- src/osipi/DRO/Model.py
+++ src/osipi/DRO/Model.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.integrate import cumtrapz
+from scipy.integrate import cumtrapz, trapz
 
 
 def tofts(cp, c_tiss, dt, datashape):
@@ -48,3 +48,30 @@ def exntended_tofts(cp, c_tiss, dt, datashape):
                 vp[j, i, k] = vp_voxel
 
     return ktrans, kep, vp
+
+
+def forward_tofts(ktrans, kep, cp, vp, dt):
+    """
+    Forward Tofts model for DCE-MRI DRO
+
+    Parameters:
+    ktrans (numpy.ndarray): The transfer constant Ktrans.
+    kep (numpy.ndarray): The rate constant kep.
+    cp (numpy.ndarray): The plasma concentration C_p(t).
+    vp (numpy.ndarray): The plasma volume fraction v_p.
+    dt (float): The time step between measurements.
+
+    Returns:
+    numpy.ndarray: The tissue concentration C_tiss(t).
+    """
+    time_points = cp.shape[-1]
+    c_tiss = np.zeros(ktrans.shape)
+    for t in range(time_points):
+        if t == 0:
+            c_tiss[..., t] = vp * cp[..., t]
+        else:
+            exp = np.exp(-kep * np.arange(t + 1)[::-1] * dt)
+            integral = trapz(cp[..., : t + 1] * exp, dx=dt, axis=-1)
+            c_tiss[..., t] = vp * cp[..., t] + ktrans * integral
+
+    return c_tiss
